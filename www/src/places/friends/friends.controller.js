ed18f3f038d6771fb55367caaa731f459c8b48ab
Stouls <NStouls@MacBook-Pro-de-Stouls.local>
2015-09-10 11:35:55 +0200

    friends-list

--- www/src/places/friends/friends.controller.js
+++ www/src/places/friends/friends.controller.js
@@ -1,6 +1,13 @@
 angular.module('places')
-	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService) {
+	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService, $http) {
+		$http.get("api/friends.json").
+			success(function(data, status, headers, config){
+				console.log(data)
 
-
+				$scope.list=data;
+			}).
+		error(function(data, status, headers, config){
+			console.log("error")
+		});
 
 	});
